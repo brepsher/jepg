8f355428c59d3f509f20ba2291956bf4767ba0e2
Brian Repsher <Brian.Repsher@motorola.com>
2013-10-10 12:06:59 -0500

    jEPG Source Uploaded

---
+++ LegacyCircle.java
@@ -0,0 +1,15 @@
+
+/**
+ * @(#)LegacyCircle.java
+ * 
+ * 
+ * @author 
+ * @version 1.00 2010/8/13
+ */
+class LegacyCircle {
+  public void draw(int x1, int y1, int x2, int y2) {
+        System.out.println("Circle with diameter from (" + x1 + ',' + y1 + ") to (" + x2 + ',' 
+          + y2 + ')');
+  }
+
+}
---
+++ shape.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)Shape.java
+ * 
+ * 
+ * @author Brian Repsher
+ * @version 1.00 2010/8/13
+ */
+ 
+ /**
+  * interface for shapes
+  */
+interface Shape {
+  void draw(int x1, int y1, int x2, int y2) ;
+
+}
